7d77f97f52366b9a342508806a8ab049d8fe1bf4
sanjaypandit1999 <sanjay7001532411@gmail.com>
2021-08-13 10:04:41 +0530

    Gambler win or lose

--- src/com/bridglabz/GamblingProb.java
+++ src/com/bridglabz/GamblingProb.java
@@ -2,8 +2,18 @@ package com.bridglabz;
 
 public class GamblingProb {
     public  static final int STAKE = 100;   // gambler's stating stack
-    public static final int BETS = 1;     // gambler's bets
+    public static final int BET = 1;     // gambler's bets
+    public void winLose(){
+        int itsWin =1;
+        int playCheck = (int)(Math.random()*10)%2;
+        if (playCheck == itsWin)
+            System.out.println("Gambler is win $1");
+        else
+            System.out.println("Gambler is loose $1");
+    }
     public static void main(String[] args) {
-            System.out.println("wlecome to gambeling game");
+        System.out.println("wlecome to gambling game");
+        GamblingProb sc = new GamblingProb();
+        sc.winLose();
     }
 }

2f74e9a4cbe4da0c3b3b7ecaf8083318e47d021f
sanjaypandit1999 <sanjay7001532411@gmail.com>
2021-08-13 09:07:49 +0530

    Stack and bet

---
+++ src/com/bridglabz/GamblingProb.java
@@ -0,0 +1,9 @@
+package com.bridglabz;
+
+public class GamblingProb {
+    public static void main(String[] args) {
+            int STAKE = 100;   // gambler's stating stack
+            int BETS = 1;     // gambler's bets
+            System.out.println("wlecome to gambeling game");
+    }
+}

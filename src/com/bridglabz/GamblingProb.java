234356fcc070deaaef5c1834e2cba320625a49ab
sanjaypandit1999 <sanjay7001532411@gmail.com>
2021-08-16 14:38:45 +0530

    luckyh or unlucky day

--- src/com/bridglabz/GamblingProb.java
+++ src/com/bridglabz/GamblingProb.java
@@ -5,7 +5,9 @@ public class GamblingProb {
     public static final int BET = 1;// gambler's bets
     public static final int itsWin = 1;
     public static final float proLoss = 0.5f;
-    public static int winningAmmount, loosingAmmout, stack,winCount,loseCount;
+    public static int winningAmmount, loosingAmmout, stack,winCount,loseCount,TOTALDAYS =30;
+    public static int arr[] = new int[TOTALDAYS];
+
 
     public static int winLose() {
         int playCheck = (int) (Math.random() * 10) % 2;
@@ -59,13 +61,40 @@ public class GamblingProb {
                 + "Total LOSES in a month: " + loseCount);
         return profitOrloss;
     }
+    public static void luckyUnluckyDay() {
+        int max = 0;
+        int min = 1500;
+        int luckiestDay = 0, unluckiestDay = 0;
 
+        for (int arr : arr) {
 
+            if (max < arr) {
+                max = arr;
+            }
+            if (min > arr)
+                min = arr;
+        }
+
+        for (int i = 0; i < TOTALDAYS; i++) {
+
+            if (arr[i] == max) {
+                luckiestDay = i + 1;
+                System.out.println("Luckiest day: day:" + luckiestDay);
+            }
+            if (arr[i] == min) {
+                unluckiestDay = i + 1;
+                System.out.println("Unluckiest day: day:" + unluckiestDay);
+            }
+        }
+        System.out.println("Profit on luckiest day: " + luckiestDay + "\n"
+                + "Loss on unluckiest day: " + unluckiestDay);
+    }
         public static void main(String[] args) {
         System.out.println("wlecome to gambling game");
         GamblingProb sc = new GamblingProb();
         sc.winLose();
         System.out.println("Gambler reached to " + sc.calStack() + " and stopped for day");
         System.out.println("Profit or loss fo 30 days : " +stakefor30days());
+        luckyUnluckyDay();
     }
 }

cfc0167a7f66f4530f30b31c88e186746868c4c5
sanjaypandit1999 <sanjay7001532411@gmail.com>
2021-08-16 14:17:00 +0530

    for 30 days prof or loss

--- src/com/bridglabz/GamblingProb.java
+++ src/com/bridglabz/GamblingProb.java
@@ -5,7 +5,7 @@ public class GamblingProb {
     public static final int BET = 1;// gambler's bets
     public static final int itsWin = 1;
     public static final float proLoss = 0.5f;
-    public static int winningAmmount, loosingAmmout, stack;
+    public static int winningAmmount, loosingAmmout, stack,winCount,loseCount;
 
     public static int winLose() {
         int playCheck = (int) (Math.random() * 10) % 2;
@@ -17,6 +17,7 @@ public class GamblingProb {
             System.out.println(" Gambler is lose ");
             stack--;
         }
+        System.out.println("kf" +stack);
         return stack;
     }
 
@@ -37,24 +38,34 @@ public class GamblingProb {
         return  stack;
     }
 
-    public static int stakeAtDay21() {
-
+    public static int stakefor30days() {
 
         int StakeAfterDay20 = 0;
         int profitOrloss;
-        for (int i = 1; i <= 20; i++) {
+        for (int i = 1; i <= 30; i++) {
             System.out.println("Day: " + i);
             calStack();
-            StakeAfterDay20 += stack ;
+            if (stack == loosingAmmout) {
+                System.out.println("Lose for day");
+                loseCount++;
+            } else if (stack == winningAmmount) {
+                System.out.println("Win for day");
+                winCount++;
+            }
+            StakeAfterDay20 += stack;
         }
         profitOrloss = StakeAfterDay20 - STAKE * 20;
+        System.out.println("Total WINS in a month: " + winCount + "\n"
+                + "Total LOSES in a month: " + loseCount);
         return profitOrloss;
     }
+
+
         public static void main(String[] args) {
         System.out.println("wlecome to gambling game");
         GamblingProb sc = new GamblingProb();
         sc.winLose();
         System.out.println("Gambler reached to " + sc.calStack() + " and stopped for day");
-        System.out.println("Profit or loss after day 20: " + stakeAtDay21());
+        System.out.println("Profit or loss fo 30 days : " +stakefor30days());
     }
 }
